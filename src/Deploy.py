c956139f7482f8aaeb6c8248e279c94bccb3e24a
Louis-VI-XVIII <pierrelouistidou@rocketmail.com>
2024-11-02 02:10:07 -0400

    new final version

--- src/Deploy.py
+++ src/Deploy.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 from Compile import compile_solidity
 
-# Load environment variables
+
 load_dotenv()
 
 ACCOUNT = os.getenv("ANVIL_ACCOUNT")
@@ -12,20 +12,20 @@ PRIVATE_KEY = os.getenv("ANVIL_PRIVATE_KEY")
 LOCAL_PROVIDER = os.getenv("LOCAL_PROVIDER")
 
 def deploy_contract(contract_file: str = "/Users/Pierre/Desktop/BLOCKCH/Web3_Homework/newContract.sol", contract_name: str = "newContract"):
-    # Compile the contract
+    
     compiled_sol = compile_solidity(contract_file)
 
-    # Extract bytecode and ABI
+   
     bytecode = compiled_sol["contracts"][contract_file][contract_name]["evm"]["bytecode"]["object"]
     abi = compiled_sol["contracts"][contract_file][contract_name]["abi"]
 
-    # Connect to the local blockchain
+    
     w3 = Web3(Web3.HTTPProvider(LOCAL_PROVIDER))
     chain_id = 31337
     contract = w3.eth.contract(abi=abi, bytecode=bytecode)
     nonce = w3.eth.get_transaction_count(ACCOUNT)
 
-    # Build the deployment transaction
+    
     transaction = contract.constructor().build_transaction({
         "chainId": chain_id,
         "gasPrice": w3.eth.gas_price,
@@ -33,19 +33,19 @@ def deploy_contract(contract_file: str = "/Users/Pierre/Desktop/BLOCKCH/Web3_Hom
         "nonce": nonce
     })
 
-    # Sign the transaction
+   
     signed_txn = w3.eth.account.sign_transaction(transaction, PRIVATE_KEY)
 
-    # Send the signed transaction
+    
     tx_hash = w3.eth.send_raw_transaction(signed_txn.raw_transaction)
 
-    # Wait for the transaction receipt to confirm deployment
+   
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
 
-    # Return the contract address and ABI
+  
     return tx_receipt.contractAddress, abi
 
 if __name__ == "__main__":
-    # Deploy the contract and print the contract address
+   
     contract_address, abi = deploy_contract()
     print(f"Contract deployed at address: {contract_address}")
--- src/Interact.py
+++ src/Interact.py
@@ -20,7 +20,7 @@ with open("src/compiled_code.json", "r") as file:
     abi = compiled_contract['contracts']['/Users/Pierre/Desktop/BLOCKCH/Web3_Homework/newContract.sol']['newContract']['abi']
 
 
-contract_address = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"  
+contract_address = "0x5FbDB2315678afecb367f032d93F642f64180aa3"  
 
 contract = w3.eth.contract(address=contract_address, abi=abi)
 
